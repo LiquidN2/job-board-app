0e583583dac56ee19442d42ca1d7705cb903a271
Hugh Nguyen <hhnguyen255@gmail.com>
2024-04-21 17:56:44 +0700

    scaffold root layout

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="f-full bg-gray-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <title>{{ $title ?? 'Page Title' }}</title>
+</head>
+<body class="h-full">
+<div class="min-h-full">
+    <livewire:components.nav />
+    <livewire:components.header />
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            {{ $slot }}
+        </div>
+    </main>
+</div>
+</body>
+</html>
